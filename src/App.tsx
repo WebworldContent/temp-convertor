e6256e825825422a7414eb5a08e8207316ccafa5
WebworldContent <jeomarkoo1@gmail.com>
2025-01-07 16:54:58 +0530

    inital commit pluse converter addition

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [cel, setCel] = useState<number>(0);
+  const [fen, setFen] = useState<number>(0);
+
+  const handleCelcius = (event) => {
+    const {value} = event.target;
+    const fern = parseInt((((parseInt(value) + 32) * 9) / 5).toString(), 10)
+    setCel(value);
+    setFen(fern);
+  }
+
+  const handleFarenhiet = (event) => {
+    const {value} = event.target;
+    const celc = parseInt((((parseInt(value) - 32) * 5) / 9).toString(), 10)
+    setFen(value);
+    setCel(celc);
+  }
+
+  return(
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+    <label>Celcius</label>
+    <input name='celcius' value={cel} onChange={handleCelcius}/>
+    <br/>
+    <label>Farenhiet</label>
+    <input name='faren' value={fen} onChange={handleFarenhiet}/>
+    </div>
+  );
+}
+
+export default App
