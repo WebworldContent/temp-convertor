648ecd0ecf622185b1018e9eebe4ea82471a3860
WebworldContent <jeomarkoo1@gmail.com>
2025-01-07 17:58:53 +0530

    small refactor on typescript part

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ function App() {
   const [cel, setCel] = useState<number>(0);
   const [fen, setFen] = useState<number>(0);
 
-  const handleCelcius = (event) => {
+  const handleCelcius = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const {value} = event.target;
-    const fern = parseInt(((parseInt(value) * (9/5)) + 32).toString(), 10);
-    setCel(value);
-    setFen(fern);
+    const toFern = parseInt(((parseInt(value) * (9/5)) + 32).toString(), 10);
+    setCel(parseInt(value));
+    setFen(toFern);
   }
 
-  const handleFarenhiet = (event) => {
+  const handleFarenhiet = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const {value} = event.target;
-    const celc = parseInt(((parseInt(value) - 32) * 5/9).toString(), 10);
-    setFen(value);
-    setCel(celc);
+    const toCelc = parseInt(((parseInt(value) - 32) * 5/9).toString(), 10);
+    setFen(parseInt(value));
+    setCel(toCelc);
   }
 
   return(
